0604d43932eae11ead4fa2be862f67604f9d8e3c
waqasnasir86 <ncwaqas@LAPTOP-M9V1SF7H.localdomain>
2020-11-03 21:10:34 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          We have changed the code @ <code>src/App.js</code> for production
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
