4c983b4be5b86ce90d6395b78716429315da750a
waqasnasir86 <ncwaqas@LAPTOP-M9V1SF7H.localdomain>
2020-11-04 01:38:51 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We have changed the code @ <code>src/App.js</code> for production
+          I am running on feature branch @ <code>src/App.js</code> for production
         </p>
         <a
           className="App-link"
